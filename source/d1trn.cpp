417de89f5ba6626176d59a24c33f9d7627e73ea2
pionere <pionere@freemail.hu>
2023-01-15 16:03:17 +0100

    paltrn 0

--- source/d1trn.cpp
+++ source/d1trn.cpp
@@ -5,18 +5,9 @@ D1Trn::D1Trn(D1Pal *pal)
 {
 }
 
-D1Trn::~D1Trn()
-{
-    delete[] translations;
-    delete resultingPalette;
-}
-
 bool D1Trn::load(QString filePath)
 {
     if (this->palette.isNull())
-        return false;
-
-    if (!QFile::exists(filePath))
         return false;
 
     QFile file = QFile(filePath);
@@ -24,18 +15,17 @@ bool D1Trn::load(QString filePath)
     if (!file.open(QIODevice::ReadOnly))
         return false;
 
-    if (file.size() != D1TRN_TRANSLATIONS_BYTES)
+    if (file.size() != D1TRN_TRANSLATIONS)
         return false;
 
-    for (int i = 0; i < D1TRN_TRANSLATIONS; i++) {
-        QByteArray translationByte = file.read(1);
-        this->translations[i] = translationByte[0];
-        this->resultingPalette->setColor(
-            i, this->palette->getColor(this->translations[i]));
-    }
+    int readBytes = file.read(this->translations, D1TRN_TRANSLATIONS);
+    if (readBytes < D1TRN_TRANSLATIONS)
+        return false;
 
-    this->modified = false;
+    this->refreshResultingPalette();
+
     this->trnFilePath = filePath;
+    this->modified = false;
     return true;
 }
 
@@ -43,22 +33,11 @@ bool D1Trn::save(QString filePath)
 {
     QFile file = QFile(filePath);
 
-    if (!file.open(QIODevice::ReadWrite))
+    if (!file.open(QIODevice::WriteOnly))
         return false;
 
-    for (int i = 0; i < D1TRN_TRANSLATIONS; i++) {
-        QByteArray colorBytes;
-        colorBytes.resize(1);
-        colorBytes[0] = this->translations[i];
-
-        if (file.write(colorBytes) == -1)
-            return false;
-    }
-
-    if (!file.flush())
-        return false;
-
-    if (file.size() != D1TRN_TRANSLATIONS_BYTES)
+    int outBytes = file.write(this->translations, D1TRN_TRANSLATIONS);
+    if (outBytes != D1TRN_TRANSLATIONS)
         return false;
 
     if (this->trnFilePath == filePath) {
@@ -104,11 +83,6 @@ void D1Trn::setTranslation(quint8 index, quint8 translation)
     this->translations[index] = translation;
 
     this->modified = true;
-}
-
-D1Pal *D1Trn::getPalette()
-{
-    return this->palette;
 }
 
 void D1Trn::setPalette(D1Pal *pal)
