85135c9d5d029b1e1a0dbbbc2f70c98f0a6c4de5
pionere <pionere@freemail.hu>
2023-01-15 09:36:29 +0100

    empty 2

--- source/celview.cpp
+++ source/celview.cpp
@@ -57,7 +57,7 @@ void CelScene::dropEvent(QGraphicsSceneDragDropEvent *event)
         filePaths.append(url.toLocalFile());
     }
     // try to insert as frames
-    if (this->activeWindow() != (QWidget *)this->view->window()) {
+    if ((QWidget *)this->activeWindow() != this->view->window()) {
         QMessageBox::warning(nullptr, "Err", "Sad day");
     }
     ((MainWindow *)this->view->window())->openImageFiles(IMAGE_FILE_MODE::AUTO, filePaths, false);
