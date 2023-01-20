ada795382e04c58180fa263490809011316f4b75
pionere <pionere@freemail.hu>
2023-01-20 17:11:40 +0100

    prog 13

--- source/progressdialog.cpp
+++ source/progressdialog.cpp
@@ -1,23 +1,32 @@
 #include "progressdialog.h"
 
+#include "ui_progressdialog.h"
+
 static ProgressDialog *theDialog;
 
 ProgressDialog::ProgressDialog(QWidget *parent)
-    : QProgressDialog("", "Cancel", 0, 0, parent)
+    : QWidget(parent)
+    , ui(new Ui::ProgressDialog())
 {
+    this->ui->setupUi(this);
+
     this->setWindowFlags((/*this->windowFlags() |*/ Qt::Tool | Qt::WindowStaysOnTopHint | Qt::WindowCloseButtonHint) & ~(Qt::WindowSystemMenuHint | Qt::WindowTitleHint | Qt::WindowContextHelpButtonHint | Qt::MacWindowToolBarButtonHint | Qt::WindowFullscreenButtonHint | Qt::WindowMinMaxButtonsHint));
-    this->setMinimumDuration(0);
-    this->setMinimum(0);
-    this->hide();
 
     theDialog = this;
 }
 
+ProgressDialog::~ProgressDialog()
+{
+    delete ui;
+}
+
 void ProgressDialog::start(const QString &label, int maxValue)
 {
-    theDialog->setLabelText(label);
-    theDialog->setMaximum(maxValue);
-    theDialog->setValue(0);
+    theDialog->ui->progressLabel->setText(label);
+    theDialog->ui->progressBar->setMaximum(maxValue);
+    theDialog->ui->progressBar->setMinimum(0);
+    theDialog->setValue_impl(0);
+    theDialog->cancelled = false;
     theDialog->show();
 }
 
@@ -28,10 +37,28 @@ void ProgressDialog::done()
 
 bool ProgressDialog::wasCanceled()
 {
-    return theDialog->wasCanceled();
+    return theDialog->wasCanceled_impl();
 }
 
 void ProgressDialog::incValue()
 {
     theDialog->setValue(theDialog->value() + 1);
+}
+
+void ProgressDialog::on_cancelPushButton_clicked()
+{
+    this->cancelled = true;
+}
+
+bool ProgressDialog::wasCanceled_impl()
+{
+    return this->cancelled;
+}
+
+void ProgressDialog::setValue_impl(int value)
+{
+    this->ui->progressBar->setValue(value);
+    this->ui->valueLabel->setText(QString("%1%%").arg(value * 100 / this->ui->progressBar->maximum()));
+    this->ui->valueLabel->repaint();
+    this->ui->progressBar->repaint();
 }
--- source/progressdialog.h
+++ source/progressdialog.h
@@ -3,16 +3,32 @@
 #include <QProgressDialog>
 #include <QWidget>
 
-class ProgressDialog : public QProgressDialog {
+namespace Ui {
+class ProgressDialog;
+} // namespace Ui
+
+class ProgressDialog : public QWidget {
     Q_OBJECT
 
 public:
     explicit ProgressDialog(QWidget *parent = nullptr);
-    ~ProgressDialog() = default;
+    ~ProgressDialog();
 
     static void start(const QString &label, int maxValue);
     static void done();
 
     static bool wasCanceled();
     static void incValue();
+
+private slots:
+    void on_cancelPushButton_clicked();
+
+private:
+    bool wasCanceled_impl();
+    void setValue_impl(int value);
+
+private:
+    Ui::ProgressDialog *ui;
+
+    bool cancelled;
 };
