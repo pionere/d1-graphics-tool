7ef3a7912825291ab2a34e4963a689db27124e65
pionere <pionere@freemail.hu>
2023-01-20 12:29:33 +0100

    prog 1

--- source/levelcelview.cpp
+++ source/levelcelview.cpp
@@ -1501,10 +1501,11 @@ void LevelCelView::upscale(const UpscaleParam &params)
     int amount = this->min->getSubtileCount();
 
     QProgressDialog progress("Upscaling...", "Cancel", 0, amount + 1, this);
-    progress.setWindowFlags((windowFlags() | Qt::WindowStaysOnTopHint | Qt::WindowCloseButtonHint) & ~(Qt::WindowSystemMenuHint | Qt::WindowContextHelpButtonHint | Qt::MacWindowToolBarButtonHint | Qt::WindowFullscreenButtonHint | Qt::WindowMinMaxButtonsHint));
+    //progress.setWindowFlags((windowFlags() | Qt::WindowStaysOnTopHint | Qt::WindowCloseButtonHint) & ~(Qt::WindowSystemMenuHint | Qt::WindowTitleHint | Qt::WindowContextHelpButtonHint | Qt::MacWindowToolBarButtonHint | Qt::WindowFullscreenButtonHint | Qt::WindowMinMaxButtonsHint));
+    progress.setWindowFlags((windowFlags() | Qt::WindowStaysOnTopHint | Qt::WindowCloseButtonHint) & ~(Qt::WindowTitleHint | Qt::WindowContextHelpButtonHint | Qt::MacWindowToolBarButtonHint | Qt::WindowFullscreenButtonHint | Qt::WindowMinMaxButtonsHint));
     progress.setWindowModality(Qt::WindowModal);
     progress.setMinimumDuration(0);
-    // progress.setWindowTitle("Upscale");
+    progress.setWindowTitle("Upscale");
     progress.setLabelText("Upscaling");
     progress.setValue(0);
     progress.show();
