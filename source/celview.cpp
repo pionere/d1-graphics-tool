414ed1c3b43ff8213c1835e6e0b07aa04a45aeaa
pionere <pionere@freemail.hu>
2023-02-27 09:02:11 +0100

    fix handling of frameIndexEdit in case framesGroupCheckBox is checked

--- source/celview.cpp
+++ source/celview.cpp
@@ -209,7 +209,7 @@ void CelView::initialize(D1Pal *p, D1Gfx *g)
     this->pal = p;
     this->gfx = g;
 
-    this->update();
+    // this->update();
 }
 
 void CelView::setPal(D1Pal *p)
@@ -667,6 +667,7 @@ void CelView::on_frameIndexEdit_returnPressed()
 
     if (this->ui->framesGroupCheckBox->isChecked() && this->gfx->getGroupCount() != 0) {
         std::pair<int, int> groupFrameIndices = this->gfx->getGroupFrameIndices(this->currentGroupIndex);
+        nextFrameIndex += groupFrameIndices.first;
         nextFrameIndex = std::max(nextFrameIndex, groupFrameIndices.first);
         nextFrameIndex = std::min(nextFrameIndex, groupFrameIndices.second);
     }
@@ -677,7 +678,8 @@ void CelView::on_frameIndexEdit_returnPressed()
 
 void CelView::on_frameIndexEdit_escPressed()
 {
-    this->ui->frameIndexEdit->setText(QString::number(this->gfx->getFrameCount() != 0 ? this->currentFrameIndex + 1 : 0));
+    // update frameIndexEdit
+    this->update();
     this->ui->frameIndexEdit->clearFocus();
 }
 
@@ -702,7 +704,8 @@ void CelView::on_groupIndexEdit_returnPressed()
 
 void CelView::on_groupIndexEdit_escPressed()
 {
-    this->ui->groupIndexEdit->setText(QString::number(this->gfx->getGroupCount() != 0 ? this->currentGroupIndex + 1 : 0));
+    // update groupIndexEdit
+    this->update();
     this->ui->groupIndexEdit->clearFocus();
 }
 
