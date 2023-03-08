a83b4d08622d23c4e708d9c972adf672d1d06a52
pionere <pionere@freemail.hu>
2023-03-08 11:51:51 +0100

    monty 4

--- source/d1dun.cpp
+++ source/d1dun.cpp
@@ -1535,7 +1535,7 @@ D1Gfx *D1Dun::loadMonster(int monsterIndex)
         } else if (result.first->trnPath != nullptr) {
             trn = new D1Trn();
             QString trnFilePath = this->assetPath + "/Monsters/" + result.first->trnPath + ".TRN";
-            if (!load(trnFilePath, this->pal)) {
+            if (!D1Trn::load(trnFilePath, this->pal)) {
                 delete trn;
                 trn = nullptr;
             }
