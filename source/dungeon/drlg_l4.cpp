44f504f38554c904e770915affc85c88ced3b4a9
pionere <pionere@freemail.hu>
2023-03-28 08:03:36 +0200

    replace the only black knight of the diablo quad

--- source/dungeon/drlg_l4.cpp
+++ source/dungeon/drlg_l4.cpp
@@ -1861,6 +1861,8 @@ static void DRLG_L4()
 		// - replace diablo
 		uint16_t* lm = (uint16_t*)pSetPieces[3]._spData;
 		lm[2 + 9 * 9 + 9 * 9 * 2 * 2 + 8 + 8 * 9 * 2] = SwapLE16((UMT_DIABLO + 1) | (1 << 15));
+		// - replace the only black knight
+		lm[2 + 9 * 9 + 9 * 9 * 2 * 2 + 4 + 6 * 9 * 2] = SwapLE16(101);
 		}
 		// DRLG_DrawMap(0);
 		DRLG_DrawMap(1);
