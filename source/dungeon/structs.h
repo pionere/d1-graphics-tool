6825842105c5867a3128155ba2eb7c0a31911c26
pionere <pionere@freemail.hu>
2023-04-11 16:15:12 +0200

    sync TFit_Shrine, TFit_Obj5 and TFit_Obj3 (game code)

--- source/dungeon/structs.h
+++ source/dungeon/structs.h
@@ -530,6 +530,18 @@ typedef struct ROOMHALLNODE {
 	int nHalldir;
 } ROOMHALLNODE;
 
+typedef struct ThemePosDir {
+	BYTE tpdx;
+	BYTE tpdy;
+	BYTE tpdvar1;
+	BYTE tpdvar2; // unused
+} ThemePosDir;
+
+/** The number of generated rooms in catacombs. */
+#define L2_MAXROOMS 32
+/** Possible matching locations in a theme room. */
+#define TH_SHRINES ((DSIZEX * DSIZEY) / sizeof(ThemePosDir))
+
 typedef struct DrlgMem {
 	union {
 		ROOMHALLNODE RoomList[L2_MAXROOMS]; // drlg_l2
@@ -537,6 +549,7 @@ typedef struct DrlgMem {
 		BYTE transDirMap[DSIZEX][DSIZEY];   // drlg_l1, drlg_l2, drlg_l3, drlg_l4 (gendung)
 		BYTE lockoutMap[DMAXX][DMAXY];      // drlg_l3
 		BYTE dungBlock[L4BLOCKX][L4BLOCKY]; // drlg_l4
+		ThemePosDir thLocs[TH_SHRINES];     // themes
 	};
 } DrlgMem;
 
