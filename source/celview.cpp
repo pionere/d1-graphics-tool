95e491ec1988562180bab2c36519e77f1a72506f
pionere <pionere@freemail.hu>
2023-05-04 15:59:50 +0200

    sel 0

--- source/celview.cpp
+++ source/celview.cpp
@@ -200,6 +200,7 @@ CelView::CelView(QWidget *parent)
 {
     this->ui->setupUi(this);
     this->ui->celGraphicsView->setScene(&this->celScene);
+    this->ui->celGraphicsView->setDragMode(QGraphicsView::RubberBandDrag);
     this->on_zoomEdit_escPressed();
     this->on_playDelayEdit_escPressed();
     QLayout *layout = this->ui->paintbuttonHorizontalLayout;
