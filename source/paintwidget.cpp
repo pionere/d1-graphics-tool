b1ae5442702987c43e7ed0f6e98f3d362636ac1e
pionere <pionere@freemail.hu>
2023-05-05 11:36:42 +0200

    hell 12

--- source/paintwidget.cpp
+++ source/paintwidget.cpp
@@ -363,11 +363,14 @@ bool PaintWidget::frameClicked(D1GfxFrame *frame, const QPoint &pos, bool first)
         QList<QGraphicsItem *> items = this->graphView->scene()->items(Qt::AscendingOrder);
         if (items.isEmpty())
             return false;
-        QString result = QString("%1:%2, %3:%4 .. %5:%6 .. %7:%8, %9:%10 .. %11:%12, %13:%14 .. %15:%16, %17:%18").arg(x).arg(y).arg(w).arg(h);
+        QString result = QString("%1:%2, %3:%4 .. %5:%6 .. scpoly %7:%8, %9:%10 .. screct %11:%12, %13:%14 .. polyrect %15:%16, %17:%18 .. grect %19:%20, %21:%22").arg(x).arg(y).arg(w).arg(h);
         result = result.arg(items[0]->x()).arg(items[0]->y());
         QPolygonF scenePolyF = items[0]->mapToScene(rectf);
         result = result.arg(scenePolyF.boundingRect().x()).arg(scenePolyF.boundingRect().y()).arg(scenePolyF.boundingRect().width()).arg(scenePolyF.boundingRect().height());
         QPolygon poly = this->graphView->mapFromScene(scenePolyF);
+        QPoint scenePTL = this->graphView->mapFromScene(x, y);
+        QPoint scenePBR = this->graphView->mapFromScene(x + w, y + h);
+        result = result.arg(scenePTL.x()).arg(scenePTL.y()).arg(scenePBR.x()).arg(scenePBR.y());
         QRect rect = poly.boundingRect();
         result = result.arg(rect.x()).arg(rect.y()).arg(rect.width()).arg(rect.height());
         QPoint topLeft = this->graphView->viewport()->mapToGlobal(rect.topLeft());
