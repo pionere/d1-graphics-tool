784a209529b48bc0bd6878654abe5d141ea441a7
pionere <pionere@freemail.hu>
2023-05-08 09:01:01 +0200

    place 3

--- source/resizedialog.cpp
+++ source/resizedialog.cpp
@@ -9,6 +9,16 @@ ResizeDialog::ResizeDialog(QWidget *parent)
     , ui(new Ui::ResizeDialog())
 {
     ui->setupUi(this);
+
+    this->ui->placementButtonGroup->setId(this->ui->topLeftPlacementRadioButton, (int)RESIZE_PLACEMENT::TOP_LEFT);
+    this->ui->placementButtonGroup->setId(this->ui->topPlacementRadioButton, (int)RESIZE_PLACEMENT::TOP);
+    this->ui->placementButtonGroup->setId(this->ui->topRightPlacementRadioButton, (int)RESIZE_PLACEMENT::TOP_RIGHT);
+    this->ui->placementButtonGroup->setId(this->ui->centerLeftPlacementRadioButton, (int)RESIZE_PLACEMENT::CENTER_LEFT);
+    this->ui->placementButtonGroup->setId(this->ui->centerPlacementRadioButton, (int)RESIZE_PLACEMENT::CENTER);
+    this->ui->placementButtonGroup->setId(this->ui->centerRightPlacementRadioButton, (int)RESIZE_PLACEMENT::CENTER_RIGHT);
+    this->ui->placementButtonGroup->setId(this->ui->bottomLeftPlacementRadioButton, (int)RESIZE_PLACEMENT::BOTTOM_LEFT);
+    this->ui->placementButtonGroup->setId(this->ui->bottomPlacementRadioButton, (int)RESIZE_PLACEMENT::BOTTOM);
+    this->ui->placementButtonGroup->setId(this->ui->bottomRightPlacementRadioButton, (int)RESIZE_PLACEMENT::BOTTOM_LEFT);
 }
 
 ResizeDialog::~ResizeDialog()
@@ -37,33 +47,7 @@ void ResizeDialog::on_resizeButton_clicked()
     params.backcolor = this->ui->backColorLineEdit->text().toUShort();
     params.rangeFrom = this->ui->rangeFromLineEdit->nonNegInt();
     params.rangeTo = this->ui->rangeToLineEdit->nonNegInt();
-    if (this->ui->topLeftPlacementRadioButton->isChecked()) {
-        params.placement = RESIZE_PLACEMENT::TOP_LEFT;
-    }
-    if (this->ui->topPlacementRadioButton->isChecked()) {
-        params.placement = RESIZE_PLACEMENT::TOP;
-    }
-    if (this->ui->topRightPlacementRadioButton->isChecked()) {
-        params.placement = RESIZE_PLACEMENT::TOP_RIGHT;
-    }
-    if (this->ui->centerLeftPlacementRadioButton->isChecked()) {
-        params.placement = RESIZE_PLACEMENT::CENTER_LEFT;
-    }
-    if (this->ui->centerPlacementRadioButton->isChecked()) {
-        params.placement = RESIZE_PLACEMENT::CENTER;
-    }
-    if (this->ui->centerRightPlacementRadioButton->isChecked()) {
-        params.placement = RESIZE_PLACEMENT::CENTER_RIGHT;
-    }
-    if (this->ui->bottomLeftPlacementRadioButton->isChecked()) {
-        params.placement = RESIZE_PLACEMENT::BOTTOM_LEFT;
-    }
-    if (this->ui->bottomPlacementRadioButton->isChecked()) {
-        params.placement = RESIZE_PLACEMENT::BOTTOM;
-    }
-    if (this->ui->bottomRightPlacementRadioButton->isChecked()) {
-        params.placement = RESIZE_PLACEMENT::BOTTOM_LEFT;
-    }
+    params.placement = (RESIZE_PLACEMENT)this->ui->placementButtonGroup->checkedId();
 
     if (params.backcolor > D1PAL_COLORS) {
         params.backcolor = D1PAL_COLORS;
