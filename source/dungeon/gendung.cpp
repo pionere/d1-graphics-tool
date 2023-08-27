cb96f8218cd56b7036dc844c3839a6a33748120f
pionere <pionere@freemail.hu>
2023-08-27 19:14:28 +0200

    fod 3

--- source/dungeon/gendung.cpp
+++ source/dungeon/gendung.cpp
@@ -213,15 +213,15 @@ void InitLvlDungeon()
 		pTiles[186][1] = 2;
 		pTiles[25][0] = 392;
 		pTiles[26][0] = 394;
-		for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
-			// pSubtiles[213][n] = pSubtiles[408][n];
-		}
-		for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
-			// pSubtiles[43][n] = pSubtiles[393][n];
-		}
-		for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
-			// pSubtiles[45][n] = pSubtiles[395][n];
-		}
+		//for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
+		//	pSubtiles[213][n] = pSubtiles[408][n];
+		//}
+		//for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
+		//	pSubtiles[43][n] = pSubtiles[393][n];
+		//}
+		//for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
+		//	pSubtiles[45][n] = pSubtiles[395][n];
+		//}
 		// fix conflict with SPA
 		pTiles[139][2] = 328;
 		pTiles[148][2] = 328;
@@ -374,18 +374,18 @@ void InitLvlDungeon()
 		// door subtiles
 		pTiles[150][2] = 15;
 		pTiles[54][0] = 173;
-		for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
-			pSubtiles[537][n] = pSubtiles[13][n];
-		}
-		for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
-			pSubtiles[539][n] = pSubtiles[17][n];
-		}
-		for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
-			pSubtiles[172][n] = pSubtiles[178][n];
-		}
-		for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
-			pSubtiles[173][n] = pSubtiles[178][n];
-		}
+		// for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
+		//	pSubtiles[537][n] = pSubtiles[13][n];
+		//}
+		//for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
+		//	pSubtiles[539][n] = pSubtiles[17][n];
+		//}
+		//for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
+		//	pSubtiles[172][n] = pSubtiles[178][n];
+		//}
+		//for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
+		//	pSubtiles[173][n] = pSubtiles[178][n];
+		//}
 		// fix conflict with SPA
 		pTiles[50][3] = 12;
 		pTiles[77][3] = 12;
@@ -502,16 +502,16 @@ void InitLvlDungeon()
 		pTiles[146][2] = 538;
 		pTiles[148][2] = 537;
 		pTiles[147][0] = 541;
-		for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
-			// uint16_t tmp = pSubtiles[531][n];
-			// pSubtiles[531][n] = pSubtiles[537][n];
-			// pSubtiles[537][n] = tmp;
-		}
-		for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
-			// uint16_t tmp = pSubtiles[541][n];
-			// pSubtiles[541][n] = pSubtiles[533][n];
-			// pSubtiles[533][n] = tmp;
-		}
+		//for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
+		//	uint16_t tmp = pSubtiles[531][n];
+		//	pSubtiles[531][n] = pSubtiles[537][n];
+		//	pSubtiles[537][n] = tmp;
+		//}
+		//for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
+		//	uint16_t tmp = pSubtiles[541][n];
+		//	pSubtiles[541][n] = pSubtiles[533][n];
+		//	pSubtiles[533][n] = tmp;
+		//}
 		// patch dMiniTiles - L3.MIN
 		// - separate subtiles for the automap
 		// pSubtiles[258][0] = 0;
@@ -685,12 +685,12 @@ void InitLvlDungeon()
 		// door subtiles - L5.MIN, L5.TIL
 		pTiles[25][1] = 205;
 		pTiles[26][0] = 208;
-		for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
-			// pSubtiles[76][n] = pSubtiles[206][n];
-		}
-		for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
-			// pSubtiles[79][n] = pSubtiles[209][n];
-		}
+		//for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
+		//	pSubtiles[76][n] = pSubtiles[206][n];
+		//}
+		//for (int n = 0; n < lengthof(pSubtiles[0]); n++) {
+		//	pSubtiles[79][n] = pSubtiles[209][n];
+		//}
 		// patch dMegaTiles - L5.TIL
 		// - fix automap of the entrance II.
 		// pTiles[52][0] = pTiles[23][0];
