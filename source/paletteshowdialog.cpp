9dcf3c9a80089e9d7238d575fd7732be59db2c06
pionere <pionere@freemail.hu>
2023-09-13 12:03:24 +0200

    shp 3

--- source/paletteshowdialog.cpp
+++ source/paletteshowdialog.cpp
@@ -61,7 +61,7 @@ void PaletteShowDialog::displayFrame()
         }
         int pos = -1;
         int dist = 0;
-        const QRgb *bits = reinterpret_cast<QRgb *>(baseImage->bits());
+        const QRgb *bits = reinterpret_cast<const QRgb *>(baseImage->bits());
         for (int n = 0; i < baseImage->width() * baseImage->height(); i++) {
             if (qAlpha(bits[n]) != 255) {
                 continue; // ignore non-opaque pixels
