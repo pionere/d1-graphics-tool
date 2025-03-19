d8da1c2c37f0140b3d9fbb4a348f8c1223f30a54
pionere <pionere@freemail.hu>
2025-03-19 12:56:31 +0100

    support the import of tileset-parts (MIN/TIL/SLA/TLA/SCEL/CEL)

--- source/importdialog.h
+++ source/importdialog.h
@@ -4,9 +4,14 @@
 
 enum class IMPORT_FILE_TYPE {
     AUTODETECT,
+    MIN,
+    TIL,
+    SLA,
+    TLA,
+    DUNGEON,
+    SCEL,
     CEL,
     CL2,
-    DUNGEON,
     FONT,
 };
 
@@ -41,9 +46,14 @@ private:
 private slots:
     void on_inputFileBrowseButton_clicked();
 
+    void on_fileTypeMINRadioButton_toggled(bool checked);
+    void on_fileTypeTILRadioButton_toggled(bool checked);
+    void on_fileTypeSLARadioButton_toggled(bool checked);
+    void on_fileTypeTLARadioButton_toggled(bool checked);
+    void on_fileTypeDUNRadioButton_toggled(bool checked);
+    void on_fileTypeSCELRadioButton_toggled(bool checked);
     void on_fileTypeCELRadioButton_toggled(bool checked);
     void on_fileTypeCL2RadioButton_toggled(bool checked);
-    void on_fileTypeDUNRadioButton_toggled(bool checked);
     void on_fileTypeFontRadioButton_toggled(bool checked);
     void on_fileTypeAutoRadioButton_toggled(bool checked);
 
