dc07af0c7937906455e7907d9ad5138a9f60d973
pionere <pionere@freemail.hu>
2025-07-16 08:17:44 +0200

    cosmetic

--- source/mainwindow.h
+++ source/mainwindow.h
@@ -129,13 +129,13 @@ public:
     void paletteWidget_callback(PaletteWidget *widget, PWIDGET_CALLBACK_TYPE type);
     void updatePalette(const D1Pal* pal);
     void remapColors(const RemapParam &params);
-    void colorModified();
     void frameClicked(D1GfxFrame *frame, const QPoint &pos, int flags);
     void pointHovered(const QPoint &pos);
     void dunClicked(const QPoint &cell, int flags);
     void dunHovered(const QPoint &cell);
     int getDunBuilderMode() const;
     void frameModified(D1GfxFrame *frame);
+    void colorModified();
 
     void initPaletteCycle();
     void nextPaletteCycle(D1PAL_CYCLE_TYPE type);
