3acd2852d6117e3b05b3d8fabd1590ec9224c70c
pionere <pionere@freemail.hu>
2026-02-20 10:20:23 +0100

    adjust progress reports during D1CelFrame/D1Cl2Frame::load - add (inactive) progress reports to debug D1Cl2Frame::computeWidthFromHeader - add (inactive) progress report to D1Cl2Frame::load when an invalid pixel (0x00) is read - inactivate the progress report in D1CelFrame::load when an invalid pixel (0x00) is read

--- source/d1cl2frame.cpp
+++ source/d1cl2frame.cpp
@@ -1,6 +1,5 @@
 #include "d1cl2frame.h"
 
-#include <QApplication>
 #include <QDataStream>
 
 #include "progressdialog.h"
@@ -12,15 +11,23 @@ unsigned D1Cl2Frame::computeWidthFromHeader(const QByteArray &rawFrameData)
     const quint16 *header = (const quint16 *)data;
     const quint8 *dataEnd = data + rawFrameData.size();
 
-    if (rawFrameData.size() < SUB_HEADER_SIZE)
+    if (rawFrameData.size() < SUB_HEADER_SIZE) {
+        // dProgress() << QString("computeWidthFromHeader 0 %1").arg(rawFrameData.size());
         return 0; // invalid header
+    }
     unsigned celFrameHeaderSize = SwapLE16(header[0]);
-    if (celFrameHeaderSize & 1)
+    if (celFrameHeaderSize & 1) {
+        // dProgress() << QString("computeWidthFromHeader 1 %1").arg(celFrameHeaderSize);
         return 0; // invalid header
-    if (celFrameHeaderSize < SUB_HEADER_SIZE)
+    }
+    if (celFrameHeaderSize < SUB_HEADER_SIZE) {
+        // dProgress() << QString("computeWidthFromHeader 2 %1").arg(celFrameHeaderSize);
         return 0; // invalid header
-    if (data + celFrameHeaderSize > dataEnd)
+    }
+    if (data + celFrameHeaderSize > dataEnd) {
+        // dProgress() << QString("computeWidthFromHeader 3 %1 %2 %3").arg((size_t)data).arg(celFrameHeaderSize).arg((size_t)dataEnd);
         return 0; // invalid header
+    }
     // Decode the 32 pixel-lines blocks to calculate the image width
     unsigned celFrameWidth = 0;
     quint16 lastFrameOffset = celFrameHeaderSize;
@@ -30,19 +37,25 @@ unsigned D1Cl2Frame::computeWidthFromHeader(const QByteArray &rawFrameData)
         if (nextFrameOffset == 0) {
             // check if the remaining entries are zero
             while (++i < celFrameHeaderSize) {
-                if (SwapLE16(header[i]) != 0)
+                if (SwapLE16(header[i]) != 0) {
+                    // dProgress() << QString("computeWidthFromHeader 4 %1").arg(header[i]);
                     return 0; // invalid header
+                }
             }
             break;
         }
 
         unsigned pixelCount = 0;
         // ensure the offsets are consecutive
-        if (lastFrameOffset >= nextFrameOffset)
+        if (lastFrameOffset >= nextFrameOffset) {
+            // dProgress() << QString("computeWidthFromHeader 5 %1 %2").arg(lastFrameOffset).arg(nextFrameOffset);
             return 0; // invalid data
+        }
         for (int j = lastFrameOffset; j < nextFrameOffset; j++) {
-            if (data + j >= dataEnd)
+            if (data + j >= dataEnd) {
+                // dProgress() << QString("computeWidthFromHeader 6 %1 %2 %3").arg((size_t)data).arg(j).arg((size_t)dataEnd);
                 return 0; // invalid data
+            }
 
             quint8 readByte = data[j];
             if (/*readByte >= 0x00 &&*/ readByte < 0x80) {
@@ -62,11 +75,16 @@ unsigned D1Cl2Frame::computeWidthFromHeader(const QByteArray &rawFrameData)
         unsigned width = pixelCount / CEL_BLOCK_HEIGHT;
         // The calculated width has to be identical for each 32 pixel-line block
         if (celFrameWidth == 0) {
-            if (width == 0)
+            if (width == 0) {
+                // dProgress() << QString("computeWidthFromHeader 7 %1 %2").arg(width).arg(pixelCount);
                 return 0; // invalid data
+            }
+            // dProgress() << QString("computeWidthFromHeader ok %1").arg(width);
         } else {
-            if (celFrameWidth != width)
+            if (celFrameWidth != width) {
+                // dProgress() << QString("computeWidthFromHeader 8 %1 %2").arg(width).arg(celFrameWidth);
                 return 0; // mismatching width values
+            }
         }
 
         celFrameWidth = width;
@@ -116,6 +134,9 @@ int D1Cl2Frame::load(D1GfxFrame &frame, const QByteArray rawData, const OpenAsPa
 
         if (/*readByte >= 0x00 &&*/ readByte < 0x80) {
             // Transparent pixels
+            // if (readByte == 0x00) {
+            //    dProgressWarn() << QString("Invalid CL2 frame data (0x00 found)");
+            // }
             for (int i = 0; i < readByte; i++) {
                 // Add transparent pixel
                 pixelLine.push_back(D1GfxPixel::transparentPixel());
